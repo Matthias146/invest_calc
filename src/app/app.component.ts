0cde3c1ca7e73406ce87f7476ea08e5867746acc
Matthias Hammelehle <209659387+Matthias146@users.noreply.github.com>
2025-05-27 22:20:34 +0200

    added some components with styles and a form to show input data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,41 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from "./user-input/user-input.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [HeaderComponent, UserInputComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  calculateInvestmentResults( data:
+    {initialInvestment: number, 
+    annualInvestment: number, 
+    expectedReturn: number, 
+    duration: number}) {
+  const annualData = [];
+  const { initialInvestment, annualInvestment, expectedReturn, duration } = data;
+
+  let investmentValue = initialInvestment;
+
+
+  for (let i = 0; i < duration; i++) {
+    const year = i + 1;
+    const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+    investmentValue += interestEarnedInYear + annualInvestment;
+    const totalInterest =
+      investmentValue - annualInvestment * year - initialInvestment;
+    annualData.push({
+      year: year,
+      interest: interestEarnedInYear,
+      valueEndOfYear: investmentValue,
+      annualInvestment: annualInvestment,
+      totalInterest: totalInterest,
+      totalAmountInvested: initialInvestment + annualInvestment * year,
+    });
+  }
+
+  return annualData;
+}
+}
